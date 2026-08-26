2e63747dfdb6fd780ada301e9f79e7d5b94aeb27
Payal Jaiswal <141217638+payaljaiswalbdh@users.noreply.github.com>
2026-08-27 05:13:44 +0530

    Time: 201 ms (5.22%), Space: 48.5 MB (5.31%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,19 +14,41 @@
  * }
  */
 class Solution {
-    int maxDia=0;
+    // approach-1
+    // int maxDia=0;
+    // private int levels(TreeNode root){
+    //     if(root==null) return 0;
+    //     return 1+Math.max(levels(root.left),levels(root.right));
+    // }
+
+    // public int diameterOfBinaryTree(TreeNode root) {
+    //     if(root==null) return 0;
+    //     int dia= levels(root.left) + levels(root.right);
+    //     maxDia=Math.max(maxDia, dia);
+    //     diameterOfBinaryTree(root.left);
+    //     diameterOfBinaryTree(root.right);
+    //     return maxDia; 
+
+
+
+    // way of writing
+int maxDia=0;
+private int solve(TreeNode root){
+    if(root==null) return 0;
+        int dia= levels(root.left) + levels(root.right);
+        maxDia=Math.max(maxDia, dia);
+        solve(root.left);
+        solve(root.right);
+        return maxDia;
+    }
+
     private int levels(TreeNode root){
         if(root==null) return 0;
         return 1+Math.max(levels(root.left),levels(root.right));
-    }
-    public int diameterOfBinaryTree(TreeNode root) {
-        if(root==null) return 0;
-        int dia= levels(root.left) + levels(root.right);
-        maxDia=Math.max(maxDia, dia);
-        diameterOfBinaryTree(root.left);
-        diameterOfBinaryTree(root.right);
-        return maxDia;
+}
 
-        
-    }
+public int diameterOfBinaryTree(TreeNode root) {
+    maxDia=0;
+    return solve(root);
+}
 }
