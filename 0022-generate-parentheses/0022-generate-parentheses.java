96218a372d0ab96d5e66e8d9e56bed2f1d101397
Payal Jaiswal <141217638+payaljaiswalbdh@users.noreply.github.com>
2026-08-29 23:38:45 +0530

    Time: 2 ms (69.84%), Space: 44.9 MB (32.63%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,38 +1,69 @@
 class Solution {
-    List<String> res=new ArrayList<>();
-    private boolean isValid(String str){
-        int count=0;
-        for(int i=0;i<str.length();i++){
-            char ch=str.charAt(i);
-            if(ch=='(')
-            count++;
-            else
-            count--;
-            if(count<0)
-            return false;
-        }
-        return count==0;
-    }
+    // approach-1;
+//     List<String> res=new ArrayList<>();
+//     private boolean isValid(String str){
+//         int count=0;
+//         for(int i=0;i<str.length();i++){
+//             char ch=str.charAt(i);
+//             if(ch=='(')
+//             count++;
+//             else
+//             count--;
+//             if(count<0)
+//             return false;
+//         }
+//         return count==0;
+//     }
 
-    private void solve(String curr, int n){
+//     private void solve(String curr, int n){
+//         if(curr.length()==2*n){
+//             if(isValid(curr)){
+//                 res.add(curr);
+//             }
+//             return;
+//         }
+//         solve(curr + "(",n);
+//         solve(curr + ")",n);
+    
+        
+//     }
+//     public List<String> generateParenthesis(int n) {
+//         String curr="";
+
+//         solve(curr ,n);
+
+//         return res;
+
+        
+//     }
+// }
+
+
+
+// approach-2
+ List<String> res=new ArrayList<>();
+ private void solve(String curr, int n,int open,int close){
         if(curr.length()==2*n){
-            if(isValid(curr)){
                 res.add(curr);
-            }
             return;
         }
-        solve(curr + "(",n);
-        solve(curr + ")",n);
-    
+        if(open<n){    
+            solve(curr + "(", n, open+1, close);
+        }
+        if(close<open){
+        solve(curr + ")", n, open, close+1);
+        }
         
     }
     public List<String> generateParenthesis(int n) {
         String curr="";
-
-        solve(curr ,n);
+         int open=0;
+         int close=0;
+        solve(curr ,n,0,0);
 
         return res;
 
         
     }
 }
+
